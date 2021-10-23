51e10fa391f5225c043579433c470ceb8fc0b5c8
624235044 <624235044@parichat.skru.ac.th>
2021-10-23 23:14:56 +0700

    add student

---
+++ src/components/StudentItem.vue
@@ -0,0 +1,67 @@
+<template>
+<div class="student-item">
+    <br />
+
+    <div class="container ">
+        <div class="row">
+            <div class="col-lg-4">
+                <img v-bind:src="student.thumbnailUrl" width="200px" /><br />
+            </div>
+            <div class="col-lg-8">
+
+                <div class="row">
+                    <div>
+                        <h2>{{student.title}}</h2>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-2 d-flex justify-content-start"><b>studentId:</b></div>
+                    <div>{{student.studentId}}</div>
+                </div>
+                <div class="row">
+                    <div class="col-2 d-flex justify-content-start"><b>studentName:</b></div>
+                    <div>{{Student.studentName}}</div>
+                </div>
+             
+                
+                <button class="btn btn-primary btn-sm" v-on:click="EditStudent(Student.studentId)" >Edit Student</button>&nbsp;
+                <button class="btn btn-danger btn-sm" v-on:click="DeleteStudent(Student.studentId)"> Delete student</button>
+                <br />
+            </div>
+        </div>
+    </div>
+    <br />
+</div>
+</template>
+
+<script>
+export default {
+    name: "StudentItem",
+    props: ["student"],
+    methods: {
+        EditStudent(studentId) {
+            console.log(studentId);
+            
+            this.$router.push('/editstudent/' + studentId)
+        },
+        DeleteStudent(studentId) {
+
+            if (confirm("Do you want to delete this student?")) {
+
+                this.$emit('delete:student',studentId);
+
+
+            }
+
+        },
+    }
+}
+</script>
+
+<style scoped>
+.book-item {
+
+    padding: 10px;
+    border-bottom: 1px #ccc dotted;
+}
+</style>
